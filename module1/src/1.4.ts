5bc3ea0da2125c9f300fb78c8224b5928cdd1f07
wpmohammad1 <alimohammadjhd@gmail.com>
2024-05-01 00:21:22 +0600

    basic data type

---
+++ module1/src/1.4.ts
@@ -0,0 +1,26 @@
+// Basic data type
+
+// string 
+let firstName : string = 'Mohammad'
+
+// number
+ let roll : number = 123
+
+//  boolean 
+let isAdmin : boolean = true;
+
+// undefined
+let x : undefined = undefined
+
+// null 
+let y : null = null
+
+let d : number;
+d = 123;
+
+// Array 
+let friends : string[] = ['Nahid', 'Rajin', 'Hasib']
+
+let eligibleNumber : number[] = [1, 2, 3]
+
+let coordinates : [number, string, boolean] = [1, 'anik', true]
