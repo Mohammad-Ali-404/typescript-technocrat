c4bd43226d424ca0e275d3ed09f883abc78127ea
wpmohammad1 <alimohammadjhd@gmail.com>
2024-05-01 22:40:16 +0600

    type alias

---
+++ module1/src/1.9.ts
@@ -0,0 +1,45 @@
+{
+    // type alias
+
+    type User ={
+        name: string;
+        age?: number;
+        gender?: string;
+        contactNo?: string;
+        address?: string;
+    }
+    const user1 :User={
+        name: 'Mohammad',
+        age: 21,
+        gender: 'Male',
+        contactNo: '017923744564',
+        address: 'jhenaidah'
+    }
+
+    const user2 :User ={
+        name: 'Anik',
+        age: 24,
+        gender: 'Male',
+        address: 'jhenaidah'
+    }
+    const user3 :User ={
+        name: 'Rajin',
+        age: 24,
+        gender: 'Male',
+        address: 'jhenaidah'
+    }
+
+
+
+    type UserName = string;
+    type isAdmin = boolean;
+    const UserName : UserName = 'Mohammad';
+    const isAdmin : isAdmin = true;
+
+
+
+
+
+
+    // 
+}
