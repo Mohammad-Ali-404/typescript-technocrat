9e97ec8fb3f79647a17521734b886dce62705ad2
wpmohammad1 <alimohammadjhd@gmail.com>
2024-05-06 13:10:07 +0600

    opp - class types module 3

---
+++ module3/3.1.ts
@@ -0,0 +1,17 @@
+{
+    // opp - class
+    // 
+    class Animal {
+        name: string;
+        species: string;
+        sound : string;
+        constructor(name: string, species: string, sound : string){
+            this.name = name;
+            this.species = species;
+            this.sound = sound;
+        }
+    }
+    const dog = new Animal('Bul dog', 'dog', 'ghew ghew')
+   
+    // 
+}
